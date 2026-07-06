000b0be04d790655754f373afb0c2d7b239b3f50
syedaalin <syedaalin@gmail.com>
2026-07-06 07:33:52 +0500

    fix: set working directory for playwright webServer to the project root

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm dev',
+    cwd: '..',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
